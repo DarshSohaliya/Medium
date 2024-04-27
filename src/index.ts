ae7a9c9e80509b9646a17ede75fcc71eab338f95
Darsh Sohaliya <darshsohaliya029290@gmail.com>
2024-04-27 23:18:35 +0530

    Create schema and migrate databse

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+
+
+const app = new Hono()
+
+
+
+app.post('/api/v1/signup' ,(c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: env.DATABASE_URL,
+}).$extends(withAccelerate())
+  return c.text("heloo")
+})
+
+app.post('/api/v1/signin' ,(c) => {
+  return c.text("heloo")
+})
+
+app.post('/api/v1/blog' ,(c) => {
+  return c.text("heloo")
+})
+
+app.put('/api/v1/blog' ,(c) => {
+  return c.text("heloo")
+})
+
+app.get('/api/v1/blog/:id' ,(c) => {
+  return c.text("heloo")
+})
+
+export default app
